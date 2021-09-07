25e12e918461d64e7ef114883be02cdd248239ac
Aminurachma <aminurachmaaisyahnilatika@gmail.com>
2021-09-07 17:18:31 +0700

    productsdetail

---
+++ app/src/main/java/com/example/jstore/ProductDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
