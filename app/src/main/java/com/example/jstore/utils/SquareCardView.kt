0b764e4523d45329b449f9616b51d137d4052255
Ari Astro <ariastronout@gmail.com>
2021-09-09 01:15:22 +0700

    Improve all UI

---
+++ app/src/main/java/com/example/jstore/utils/SquareCardView.kt
@@ -0,0 +1,19 @@
+package com.example.jstore.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.card.MaterialCardView
+
+class SquareCardView : MaterialCardView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(measuredWidth, measuredWidth)
+    }
+}
