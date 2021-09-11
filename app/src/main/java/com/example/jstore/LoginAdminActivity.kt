9768c4eca0894a777bd57551b8e9a7818ac127af
Aminurachma <aminurachmaaisyahnilatika@gmail.com>
2021-09-11 15:34:09 +0700

    loginadmin

---
+++ app/src/main/java/com/example/jstore/LoginAdminActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_admin)
+    }
+}
