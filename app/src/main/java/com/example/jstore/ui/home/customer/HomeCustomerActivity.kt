29e751307dcdd2283befaaec599e816cf0c5bd73
Ari Astro <ariastronout@gmail.com>
2021-09-14 00:11:10 +0700

    Setup bottom navigation with navigation component

--- app/src/main/java/com/example/jstore/ui/home/customer/HomeCustomerActivity.kt
+++ app/src/main/java/com/example/jstore/ui/home/customer/HomeCustomerActivity.kt
@@ -2,11 +2,32 @@ package com.example.jstore.ui.home.customer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.jstore.R
+import com.example.jstore.databinding.ActivityHomeCustomerBinding
 
 class HomeCustomerActivity : AppCompatActivity() {
+
+    private var _binding: ActivityHomeCustomerBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_customer)
+        _binding = ActivityHomeCustomerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUI()
+
     }
+
+    private fun setupUI() {
+        binding.bottomNav.setupWithNavController(findNavController(R.id.fragmentContainerView))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
